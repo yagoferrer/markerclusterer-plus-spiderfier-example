73083eb48c2f36cf3d89712f8b80ce35ff243080
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-01 16:18:39 -0800

    Feature: Adds cleanup and code structure.

--- src/demo.js
+++ src/demo.js
@@ -1,78 +1,89 @@
-data = [
-    {lat: 37.4419, lon: -122.1419, title: "location 1"},
-    {lat: 37.4419, lon: -122.1419, title: "location 2"},
-    {lat: 37.4419, lon: -122.1419, title: "Marker same location 3"},
-    {lat: 37.4419, lon: -122.1419, title: "Marker same location 4"},
-    {lat: 37.4419, lon: -122.1419, title: "Marker same location 5"},
-    {lat: 37.4419, lon: -122.1419, title: "Marker same location 6"},
-    {lat: 37.4419, lon: -122.1419, title: "Marker same location 7"},
-    {lat: 37.4419, lon: -122.1419, title: "Marker same location 8"},
-    {lat: 37.4419, lon: -122.1419, title: "Marker same location 9"}
-]
-
-
-
-var minClusterZoom = 15;
-
-function initialize() {
-
-    var center = new google.maps.LatLng(37.4419, -122.1419);
+'use strict';
+(function(){
 
     var gm = google.maps;
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var config = {
+        el: 'map',
+        lat: 37.4419,
+        lon: -122.1419,
         zoom: 15,
-        center: center,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+        minZoom: 15,
+        type: google.maps.MapTypeId.ROADMAP
+    };
 
-
-    var options =  {
+    var spiderConfig = {
         keepSpiderfied: true,
         event: 'mouseover'
     };
 
+    // A list of Markers with the same location
+    var data = [
+        {lat: 37.4419, lon: -122.1419, title: 'location 1'},
+        {lat: 37.4419, lon: -122.1419, title: 'location 2'},
+        {lat: 37.4419, lon: -122.1419, title: 'Marker same location 3'},
+        {lat: 37.4419, lon: -122.1419, title: 'Marker same location 4'},
+        {lat: 37.4419, lon: -122.1419, title: 'Marker same location 5'},
+        {lat: 37.4419, lon: -122.1419, title: 'Marker same location 6'},
+        {lat: 37.4419, lon: -122.1419, title: 'Marker same location 7'},
+        {lat: 37.4419, lon: -122.1419, title: 'Marker same location 8'},
+        {lat: 37.4419, lon: -122.1419, title: 'Marker same location 9'}
+    ];
 
-    var oms = new OverlappingMarkerSpiderfier(map, options);
 
-    var markers = [];
+    function initialize() {
 
-    for (var x in data) {
-
-        var loc = new gm.LatLng(data[x].lat, data[x].lon);
-
-        var marker = new gm.Marker({
-            position: loc,
-            title: data[x].title,
-            map: map
+        var map = new gm.Map(document.getElementById(config.el), {
+            zoom: config.zoom,
+            center: new gm.LatLng(config.lat, config.lon),
+            mapTypeId: config.type
         });
 
-        marker.desc = data[x].title;
 
-        markers.push(marker); // Saving Markers
+        var markerSpiderfier = new OverlappingMarkerSpiderfier(map, spiderConfig);
 
-        oms.addMarker(marker);  // <-- here
+        var markers = [];
+
+
+        for (var x in data) {
+
+            var loc = new gm.LatLng(data[x].lat, data[x].lon);
+
+            var marker = new gm.Marker({
+                position: loc,
+                title: data[x].title,
+                map: map
+            });
+
+            marker.desc = data[x].title;
+
+            markers.push(marker); // Saving Markers
+
+            markerSpiderfier.addMarker(marker);  // Adds the Marker to OverlappingMarkerSpiderfier
+        }
+
+
+        var iw = new gm.InfoWindow();
+
+        markerSpiderfier.addListener('click', function(marker, e) {
+            iw.setContent(marker.title);
+            iw.open(map, marker);
+        });
+
+        markerSpiderfier.addListener('spiderfy', function(markers) {
+            iw.close();
+        });
+
+
+        var markerCluster = new MarkerClusterer(map, markers);
+
+        markerCluster.setMaxZoom(config.minZoom);
+        
     }
 
+    gm.event.addDomListener(window, 'load', initialize);
+
+})();
 
 
-    var iw = new gm.InfoWindow();
 
-    oms.addListener('click', function(marker, event) {
-        iw.setContent(marker.title);
-        iw.open(map, marker);
-    });
-
-    oms.addListener('spiderfy', function(markers) {
-        iw.close();
-    });
-
-
-    var markerCluster = new MarkerClusterer(map, markers);
-    markerCluster.setMaxZoom(minClusterZoom);
-
-
-}
-
-
-google.maps.event.addDomListener(window, 'load', initialize);
